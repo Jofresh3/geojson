af3f0893a0d78ea796f2b6a2aae9209965973a20
Jofresh3 <89497325+Jofresh3@users.noreply.github.com>
2023-12-28 14:50:33 +0900

    Update 6_6️⃣매출 계산기.py

--- pages/6_6️⃣매출 계산기.py
+++ pages/6_6️⃣매출 계산기.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 def main():
     st.title("streamlit Button widget")
-    price = st.slider("단가:",1000,100000,value=5000)
+    price = st.slider("객단가:",1000,100000,value=5000)
     total_sales = st.slider("전체 판매 개수:",1,1000,value=500)
 
     revenue = price * total_sales
